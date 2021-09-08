43329aae882c2e1ca0b40f7df53f78861c1d0354
Jason Corchado <cpsrider89@gmail.com>
2021-09-07 20:12:22 -0400

    populated skills and contacts beginning css

--- portfolio/src/components/contact.js
+++ portfolio/src/components/contact.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import github from '../imgs/Icons/icons8-github-384.png';
+import linkedin from '../imgs/Icons/icons8-linkedin-480.png';
+import gmail from '../imgs/Icons/icons8-gmail-480.png';
 
 function Contact() {
     return(
         <div className='Contact'>
-            <a href='https://github.com/JasonCorchado' target="_blank" rel="noreferrer">Github</a>
-            <a href='https://www.linkedin.com/in/jason-corchado/' target="_blank" rel="noreferrer">LinkedIn</a>
-            <p>corchadojason@gmail.com</p>
+            <div>
+                <img src={github} alt='github'/>
+                <a href='https://github.com/JasonCorchado' target="_blank" rel="noreferrer">Github</a>
+            </div>
+            <div>
+                <img src={linkedin} alt='linkedin'/>
+                <a href='https://www.linkedin.com/in/jason-corchado/' target="_blank" rel="noreferrer">LinkedIn</a>
+            </div>
+            <div>
+                <img src={gmail} alt='gmail'/>
+                <a href="mailto:corchadojason@gmail.com?subject=Message%20from%20portfolio%20site" target="_blank" rel="noopener noreferrer">Corchadojason@gmail.com</a>
+            </div>
         </div>
     );
 }
--- portfolio/src/components/skills.js
+++ portfolio/src/components/skills.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import Css3 from '../imgs/Icons/Css3.png';
+import Git from '../imgs/Icons/Git.png';
+import HTMLimg from '../imgs/Icons/HTML.png';
+import Java from '../imgs/Icons/Java.png';
+import Javascript from '../imgs/Icons/Javascript.png';
+import PostgreSQL from '../imgs/Icons/Postgresql.png';
+import Python from '../imgs/Icons/Python.png';
+import Reactimg from '../imgs/Icons/React.png';
+import Redux from '../imgs/Icons/Redux.png';
+import Spring from '../imgs/Icons/Spring Framework.png';
+
 
 function Skills() {
+
+    const skills1 = [
+        {name:"CSS", img:Css3},
+        {name:"Git", img:Git},
+        {name:"HTML", img:HTMLimg},
+        {name:"Java", img:Java},
+        {name:"Javascript", img:Javascript}]
+
+    const skills2 = [
+        {name:"PostgreSQL", img:PostgreSQL},
+        {name:"Python", img:Python},
+        {name:"React", img:Reactimg},
+        {name:"Redux", img:Redux},
+        {name:"Spring Framework", img:Spring}]    
+
     return(
-        <div>
-            <h1>soon</h1>
+        <div className='Skills'>
+            <div className='Row1'>
+                {skills1.map( skill => (
+                    <div>
+                        <img src={skill.img} alt={skill.name}/>
+                        <p>{skill.name}</p>
+                    </div>
+                ))}
+
+            </div>
+            <div className='Row2'>
+            {skills2.map( skill => (
+                    <div>
+                        <p>{skill.name}</p>
+                        <img src={skill.img} alt={skill.name}/>
+                    </div>
+                ))}
+
+            </div>
+
         </div>
     );
 }
