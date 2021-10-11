ed4fb153bbe22b03c4f858d30f227141e68361c4
Jason Corchado <cpsrider89@gmail.com>
2021-10-11 19:44:52 -0400

    fleshed out abt section and added logo

--- portfolio/src/components/about.js
+++ portfolio/src/components/about.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Divider from '../imgs/504-5045315_white-divider-png.png'
 
 function About() {
     return(
         <div className='About'>
             <h1>Hi I'm Jason Corchado</h1>
-            <p>I'm a Full Stack developer from New York, who is passionate about building WebApps. Some language I work with are JavaScript, React, css, and Html. I enjoy learning more about code, and hope to further my knowledge within my Field. I also have learned to use Python and plan on furthering my knowledge with the language.  </p> 
+            <p>I'm a Full Stack developer from New York. I'm passionate about this field and enjoy learning as well as growing within this field. just like many New Yorkers I'm determined to be the best version of myself every day, and I think that shows in my work ethic as well as my code.</p>
+            <img src={Divider} alt='divider' />
+            <p>I believe the web is the present and future of our civilization, I intend to use my skills to help create websites and webapps that help companies represent their buisness through design and a user expierance that is easy to use and navigate. I have languages that I am already familiar and proficent with but I'm willing and capable of expanding that toolset.</p>
+            <img src={Divider} alt='divider' />
+            <p>On that note feel free to contact me if you would like to work with me or have any questions about what I can offer. </p> 
+            
         </div>
     );
 };
--- portfolio/src/components/navbar.js
+++ portfolio/src/components/navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import Jc from '../imgs/Icons/JC.gif'
 
 function Navbar() {
     return (
         <div className='Navbar'>
+            <img src={Jc} alt="gif logo"/>
             <Link to='/'>Home</Link>
             <Link to='/skills'>Skills</Link>
             <Link to='/projects'>Projects</Link>
